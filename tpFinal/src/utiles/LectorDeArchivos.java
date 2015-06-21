cb87ab40658e9b398a436af03864195d83434a16
leonelcampos <soy.leonelcampos@gmail.com>
2015-06-21 10:41:51 -0300

    Se modifico el test del LectorDeArchivosTest, ahora comprueba la cantidad de archivos zip leidos, no sólo que sean Files. Y se agregó nuevo test para comprobar la cantidad de archivos dentro de un zip

--- tpFinal/src/utiles/LectorDeArchivos.java
+++ tpFinal/src/utiles/LectorDeArchivos.java
@@ -1,15 +1,28 @@
 package utiles;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 public class LectorDeArchivos {
 
-	public File[] getListaDeArchivos(String path) {
+	public ZipFile[] getArchivosZip(String path) throws ZipException, IOException {
 		
 		File dir = new File(path);
 		File[] listaArchivosEnDirectorio = dir.listFiles();
+		ZipFile[] archivoZip = new ZipFile[listaArchivosEnDirectorio.length];
 		
-		return listaArchivosEnDirectorio;
+		for(int i= 0; i< listaArchivosEnDirectorio.length ; i++){
+			archivoZip[i] = new ZipFile(listaArchivosEnDirectorio[i]);
+		}
+		return archivoZip;
 	}
 
+	public Enumeration<? extends ZipEntry> descomprimirZip(ZipFile zipFile) {
+		
+		return zipFile.entries();
+	}
 }
--- tpFinal/test/utiles/LectorDeArchivosTest.java
+++ tpFinal/test/utiles/LectorDeArchivosTest.java
@@ -1,20 +1,56 @@
 package utiles;
 
-import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class LectorDeArchivosTest {
 	
 	@Test
-	public void cuandoLeoUnDirectorioTodosLosArchivosZipDelDirectorioSonLeidos(){
+	public void cuandoLeoUnDirectorioTodosLosArchivosZipDelDirectorioSonLeidos() throws ZipException, IOException{
 		
 		LectorDeArchivos lectorDeArchivos = new LectorDeArchivos();
 		
-		File[] listaDeArchivosEnDirectorio = lectorDeArchivos.getListaDeArchivos("documentos");
+		ZipFile[] listaDeArchivosZipEnDirectorio = lectorDeArchivos.getArchivosZip("documentos");
 		
-		int cantidadDeArchivosLeidos = 3;
+		int cantidadDeArchivosZipLeidos = 3;
 		
-		Assert.assertEquals(cantidadDeArchivosLeidos, listaDeArchivosEnDirectorio.length);
+		Assert.assertEquals(cantidadDeArchivosZipLeidos, listaDeArchivosZipEnDirectorio.length);
 	}
+	
+	@Test
+	public void cuandoLeoUnArchivoZipTodosLosArchivosCSVDelArchivoSonLeidos() throws ZipException, IOException{
+		
+		LectorDeArchivos lectorDeArchivosZip = new LectorDeArchivos();
+		
+		ZipFile[] listaDeArchivosZipEnDirectorio = lectorDeArchivosZip.getArchivosZip("documentos");
+		
+		Enumeration<? extends ZipEntry> listaDeArchivosCSVEnZip = lectorDeArchivosZip.
+				descomprimirZip(listaDeArchivosZipEnDirectorio[0]);
+		
+		int cantidadDeArchivosCSVQueSeEsperaQueLea = 4;
+		
+		int cantidadDeArchivosCSVLeidos = contarArchivosCSVLeidos(listaDeArchivosCSVEnZip);
+		
+		Assert.assertEquals(cantidadDeArchivosCSVQueSeEsperaQueLea, cantidadDeArchivosCSVLeidos);
+	}
+
+	private int contarArchivosCSVLeidos(
+			Enumeration<? extends ZipEntry> listaDeArchivosCSVEnZip) {
+		int archivosCSVLeidos = 0;
+		
+		while(listaDeArchivosCSVEnZip.hasMoreElements()){
+			listaDeArchivosCSVEnZip.nextElement();
+			archivosCSVLeidos++;
+		}
+		return archivosCSVLeidos;
+	}
+	
+	//agregar test de validacion de que el directorio exista.
 }
+
