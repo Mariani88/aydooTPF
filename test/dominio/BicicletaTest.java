a3c5aee0f2bba0cedbfa39c8675c93ec4a20914f
Mariani88 <pm_mariani@hotmail.com>
2015-06-23 20:48:23 -0300

    test verde para que dos bicicletas iguales devuelvan mismo hashCode

--- test/dominio/BicicletaTest.java
+++ test/dominio/BicicletaTest.java
@@ -16,4 +16,14 @@ public class BicicletaTest {
 		Assert.assertTrue(bicicleta1.equals(bicicleta2));
 		Assert.assertFalse(bicicleta1.equals(bicicleta3));
 	}	
+	
+	
+	@Test
+	public void dosBicicletasDebenDevolverMismoHashCode (){
+		
+		Bicicleta bicicleta1 = new Bicicleta (1, null);
+		Bicicleta bicicleta2 = new Bicicleta (1, null);
+		
+		Assert.assertEquals(bicicleta1.hashCode(), bicicleta2.hashCode());
+	}
 }
