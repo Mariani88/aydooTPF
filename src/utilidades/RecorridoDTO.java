92846c6556d0fa1154a71f1b645110833ad07d66
Mariani88 <pm_mariani@hotmail.com>
2015-06-24 22:58:11 -0300

    corrijo hashCode en RecorridoDTO

--- src/utilidades/RecorridoDTO.java
+++ src/utilidades/RecorridoDTO.java
@@ -34,7 +34,6 @@ public class RecorridoDTO {
 	
 	public int hashCode (){
 		
-		return this.idEstacionDestino.hashCode()
-				+ this.idEstacionDestino.hashCode();
+		return this.idEstacionOrigen.hashCode();
 	}
 }
