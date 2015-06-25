1b19142c4f21afb030bc23186f2bfa1d17722af1
Mariani88 <pm_mariani@hotmail.com>
2015-06-25 18:06:24 -0300

    test verde para generar archivo yml

--- src/utilidades/GestorDeArchivos.java
+++ src/utilidades/GestorDeArchivos.java
@@ -1,7 +1,13 @@
 package utilidades;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
 
+import dominio.InformacionEstadistica;
 import excepciones.DirectorioNoExisteExcepcion;
 
 public class GestorDeArchivos {
@@ -14,9 +20,68 @@ public class GestorDeArchivos {
 		
 		File file = new File (directorioDeTrabajo);
 		
-		if ( file.exists()) this.directorioDeTrabajo = directorioDeTrabajo;
+		if ( file.exists()) {
+			this.directorioDeTrabajo = directorioDeTrabajo;
+		}
 		else throw new DirectorioNoExisteExcepcion();
 	}
-	
-	
+
+	public void crearYMLCon(InformacionEstadistica info) throws IOException {
+		
+		File archivoYML = new File (this.directorioDeTrabajo + "/estadisticas.yml");
+		FileWriter fw = new FileWriter (archivoYML);
+		BufferedWriter bw = new BufferedWriter (fw);
+		PrintWriter pw = new PrintWriter (bw);
+		
+		pw.println("Bicicletas mas usadas: ");
+		this.escribirIdsBicicletasMaximas(info, pw);
+		
+		pw.println("Bicicletas menos usadas: ");
+		this.escribirIdsBicicletasMinimas(info, pw);
+		
+		pw.println("Recorridos mas realizados: ");
+		this.escribirIdRecorridosMasRealizados (info, pw);
+		
+		pw.println("Tiempo promedio de uso: " + info.getTiempoPromedio());
+		pw.close();
+	}
+
+	private void escribirIdRecorridosMasRealizados(InformacionEstadistica info,
+			PrintWriter pw) {
+		
+		Iterator <RecorridoDTO> iterador = info.recorridosMasRealizados().iterator();
+		
+		while (iterador.hasNext()){
+			
+			RecorridoDTO recorrido = iterador.next();
+			pw.println("id origen:" + recorrido.getIdEstacionOrigen());
+			pw.println("id destino" + recorrido.getIdEstacionDestino());
+			pw.println("");
+		}
+	}
+
+	private void escribirIdsBicicletasMinimas(InformacionEstadistica info,
+			PrintWriter pw) {
+		
+		Iterator <Integer> iterador = info.bicicletasMenosUsadas().iterator();
+		
+		while (iterador.hasNext()){
+			pw.println("id:" + iterador.next());
+		}
+		
+		pw.println("");
+		
+	}
+
+	private void escribirIdsBicicletasMaximas(InformacionEstadistica info,
+			PrintWriter pw) {
+		
+		Iterator <Integer> iterador = info.bicicletasMasUsadas().iterator();
+		
+		while (iterador.hasNext()){
+			pw.println("id:" + iterador.next());
+		}
+		
+		pw.println("");
+	}	
 }
--- test/utilidades/GestorDeArchivosTest.java
+++ test/utilidades/GestorDeArchivosTest.java
@@ -1,5 +1,13 @@
 package utilidades;
 
+import java.io.File;
+
+
+
+
+import java.io.IOException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import dominio.InformacionEstadistica;
@@ -7,28 +15,29 @@ import excepciones.DirectorioNoExisteExcepcion;
 
 public class GestorDeArchivosTest {
 	
+	private String directorio = "documentos";
+	
 	@Test(expected=DirectorioNoExisteExcepcion.class)
 	public void directorioInexistenteDeDevolverExcepcion () throws DirectorioNoExisteExcepcion{
 		
 		GestorDeArchivos gestor = new GestorDeArchivos ("hola");
-		
-		
 	}	
 	
-	/*@Test
-	public void generarYMLDebeCrearYML (){
+	@Test
+	public void generarYMLDebeCrearYML () throws DirectorioNoExisteExcepcion, IOException{
 		
-		GestorDeArchivos gestor = new GestorDeArchivos ();
+		GestorDeArchivos gestor = new GestorDeArchivos (this.directorio);
 		InformacionEstadistica info = new InformacionEstadistica ();
 		
+		info.guardarBicicletasMasUsadas(3, 10);
+		info.guardarBicicletasMenosUsadas(1, 2);
+		info.guardarRecorridoMasRealizado(new RecorridoDTO (1,2), 2);
+		info.setTiempoPromedio(30);
 		
 		gestor.crearYMLCon (info);
 		
-		File
-		*/	
+		File file = new File (this.directorio + "/estadisticas.yml");
 			
-		
-		
-	
-
+		Assert.assertTrue(file.exists());
+	}
 }
