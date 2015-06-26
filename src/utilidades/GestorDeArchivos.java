1761a77d646c17b1c049e57d8f5419955740f71c
leonelcampos <soy.leonelcampos@gmail.com>
2015-06-25 23:02:00 -0300

    Se agergo tes en verde de obtenerListaDeBicicletas.

--- src/utilidades/GestorDeArchivos.java
+++ src/utilidades/GestorDeArchivos.java
@@ -1,60 +1,108 @@
 package utilidades;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
+import dominio.Bicicleta;
+import dominio.Estacion;
 import dominio.InformacionEstadistica;
+import dominio.Recorrido;
 
 public class GestorDeArchivos {
+	
+	
+	private Enumeration<? extends ZipEntry> archivosCSV;
+	private ZipEntry archivoCSV;
+	private ZipFile archivoZip;
+	private Boolean esPrimeraLecturaDelArchivoZip;
+	private Boolean esPrimeraLecturaDelArchivoCSV;
+	private BufferedReader bufferDeLectura;
+	
 	
 	public ZipFile[] obtenerArchivosZip(String path) throws ZipException, IOException {
 		File directorio = new File(path);
 		comprobarPath(directorio.toPath());
 		File[] listaArchivosEnDirectorio = directorio.listFiles();
-		ZipFile[] archivoZip = new ZipFile[listaArchivosEnDirectorio.length];
+		ZipFile[] archivosZip = new ZipFile[listaArchivosEnDirectorio.length];
 		
 		for(int i= 0; i< listaArchivosEnDirectorio.length ; i++){
-			archivoZip[i] = new ZipFile(listaArchivosEnDirectorio[i]);
+			archivosZip[i] = new ZipFile(listaArchivosEnDirectorio[i]);
 		}
+		return archivosZip;
+	}
+	
+	public ZipFile getArchivoZip() {
 		return archivoZip;
 	}
 
-	public Enumeration<? extends ZipEntry> leerArchivosCSVContenidosEnZip(ZipFile zipFiles) {
-		Enumeration<? extends ZipEntry> listaDeArchivosCSVEnZip =  zipFiles.entries();
+	public void asignarArchivoZipParaProcesar(ZipFile archivoZip) {
+		esPrimeraLecturaDelArchivoZip = Boolean.TRUE;
+		esPrimeraLecturaDelArchivoCSV = Boolean.TRUE;
+		this.archivoZip = archivoZip;
+	}
+
+	public List<Bicicleta> obtenerListaDeBicicletas(Integer cantidad) throws IOException, ParseException{
+		List<Bicicleta> bicicletas = new ArrayList<Bicicleta>();
+		String registroLeido = null;
+		Bicicleta bicicleta;
 		
-		return listaDeArchivosCSVEnZip;
+		if(esPrimeraLecturaDelArchivoZip){
+			archivosCSV = leerArchivosCSVContenidosEnZip(archivoZip);
+			esPrimeraLecturaDelArchivoZip = Boolean.FALSE;
+		}
+		if(esPrimeraLecturaDelArchivoCSV){
+			archivoCSV = archivosCSV.nextElement();
+			InputStream stream = archivoZip.getInputStream(archivoCSV);
+		    InputStreamReader inputStreamReader = new InputStreamReader(stream);
+		    bufferDeLectura = new BufferedReader(inputStreamReader);
+		    bufferDeLectura.readLine();
+		    esPrimeraLecturaDelArchivoCSV = Boolean.FALSE;
+		}
+		while(bicicletas.size() < cantidad && 
+				(registroLeido = bufferDeLectura.readLine()) != null){
+			bicicleta = generarBicicleta(registroLeido);
+			bicicletas.add(bicicleta);
+		}
+	
+		return bicicletas;
+	}
+	
+	private Bicicleta generarBicicleta(String registroLeido) throws ParseException  {
+		String[] campos = registroLeido.split(";");
+		
+		Integer bicicletaId = Integer.parseInt(campos[1]);
+		Integer estacionOrigenId = Integer.parseInt(campos[3]);
+		String estacionOrigenNombre = campos[4];
+		Integer estacionDestinoId = Integer.parseInt(campos[6]);
+		String estacionDestinoNombre = campos[7];
+		Integer minutosRecorridos = Integer.parseInt(campos[8]);
+		
+		Estacion estacionOrigen = new Estacion(estacionOrigenId, estacionOrigenNombre);
+		Estacion estacionDestino = new Estacion(estacionDestinoId,estacionDestinoNombre);
+		Recorrido recorrido = new Recorrido(estacionOrigen, estacionDestino);
+		recorrido.setMinutosRecorridos(minutosRecorridos);
+		Bicicleta bicicleta = new Bicicleta(bicicletaId,recorrido);
+		
+		return bicicleta;
 	}
 
-	public void validarQueElArchivoSeaCSV(ZipEntry archivoCSV) {
-		String name = archivoCSV.getName();
-		if(!name.contains(".csv")){
-			throw new IllegalArgumentException("El archivo con el nombre "+name+
-					" no es un archivo csv");
-		}
-	}
-
-	private void comprobarPath(Path path) {
-		try{
-			Boolean elDirectorioEsValido = ((Boolean) Files.getAttribute(path,
-					"basic:isDirectory", LinkOption.NOFOLLOW_LINKS));
-			if (!elDirectorioEsValido) {
-				throw new IllegalArgumentException("El Path: "+ path + " no es un directorio");
-			}
-		} catch (IOException ioe) {
-			throw new IllegalArgumentException("El directorio con el path: "+path+"no existe" );
-		}
-	}
 	
 	public void crearYMLCon(InformacionEstadistica info, String directorioDeTrabajo) throws IOException {
 		
@@ -100,7 +148,6 @@ public class GestorDeArchivos {
 		}
 		
 		pw.println("");
-		
 	}
 
 	private void escribirIdsBicicletasMaximas(InformacionEstadistica info,
@@ -114,5 +161,23 @@ public class GestorDeArchivos {
 		
 		pw.println("");
 	}	
+	
+	private Enumeration<? extends ZipEntry> leerArchivosCSVContenidosEnZip(ZipFile zipFiles) {
+		Enumeration<? extends ZipEntry> listaDeArchivosCSVEnZip =  zipFiles.entries();
+		
+		return listaDeArchivosCSVEnZip;
+	}
+	
+	private void comprobarPath(Path path) {
+		try{
+			Boolean elDirectorioEsValido = ((Boolean) Files.getAttribute(path,
+					"basic:isDirectory", LinkOption.NOFOLLOW_LINKS));
+			if (!elDirectorioEsValido) {
+				throw new IllegalArgumentException("El Path: "+ path + " no es un directorio");
+			}
+		} catch (IOException ioe) {
+			throw new IllegalArgumentException("El directorio con el path: "+path+"no existe" );
+		}
+	}
 	
 }
--- test/utilidades/GestorDeArchivosTest.java
+++ test/utilidades/GestorDeArchivosTest.java
@@ -2,17 +2,23 @@ package utilidades;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
 import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import dominio.Bicicleta;
 import dominio.InformacionEstadistica;
 
 public class GestorDeArchivosTest {
 	
+	private static final String PATH_ARCHIVOS_YML = "documentos";
 	private static final String PATH_INVALIDO = "path invalido";
 	private static final String PATH_ARCHIVOS_ZIP = "documentos/archivos zip para test";
+	
 	
 	@Test(expected= IllegalArgumentException.class)
 	public void cuandoSeQuiereObtenerArchivosDeUnDirectorioInexistenteEsperoExcepcion()
@@ -33,12 +39,28 @@ public class GestorDeArchivosTest {
 		info.guardarRecorridoMasRealizado(new RecorridoDTO (1,2), 2);
 		info.setTiempoPromedio(30);
 		
-		gestor.crearYMLCon(info, PATH_ARCHIVOS_ZIP); 
+		gestor.crearYMLCon(info, PATH_ARCHIVOS_YML); 
 		
-		File file = new File (PATH_ARCHIVOS_ZIP+ "/estadisticas.yml");
+		File file = new File ("documentos/estadisticas.yml");
 			
 		Assert.assertTrue(file.exists());
 	}
 	
 	
+	@Test
+	public void obtenerListaDeBicicletasDevuelveUnaListaDeBicicletas() throws ZipException,
+		IOException, ParseException{
+		
+		GestorDeArchivos gestor = new GestorDeArchivos();
+		
+		ZipFile[] archivosZip = gestor.obtenerArchivosZip(PATH_ARCHIVOS_ZIP);
+		gestor.asignarArchivoZipParaProcesar(archivosZip[0]);
+		
+		int cantidadDeBicicletasAObtener = 500;
+		
+		List<Bicicleta> bicicletas = gestor.obtenerListaDeBicicletas(cantidadDeBicicletasAObtener);
+		
+		Assert.assertEquals(cantidadDeBicicletasAObtener, bicicletas.size());
+	}
+
 }
