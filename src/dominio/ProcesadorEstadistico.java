6e08fcae806b588beb17fac325207fefdcfe335e
Mariani88 <pm_mariani@hotmail.com>
2015-06-25 23:36:55 -0300

    agrego logica a ProcesadorEstadistico

--- src/dominio/ProcesadorEstadistico.java
+++ src/dominio/ProcesadorEstadistico.java
@@ -1,5 +1,109 @@
 package dominio;
 
+import java.io.File;
+import java.util.Scanner;
+
+import javax.management.monitor.Monitor;
+
+import excepciones.DirectorioNoExisteExcepcion;
+import utilidades.GeneradorDeEstadistica;
+import utilidades.GestorDeArchivos;
+import utilidades.MonitorDeDirectorio;
+
 public class ProcesadorEstadistico {
 
+	private static MonitorDeDirectorio monitorDeDirectorio;
+	private static GeneradorDeEstadistica generadorDeEstadisticas;
+	private static GestorDeArchivos gestorDeArchivos;
+	private static Scanner escaner = new Scanner (System.in);
+	
+	public static void main (String args []){
+		
+		inicializar();
+		monitorDeDirectorio.monitorear ();
+		menu();
+		escaner.close();
+			
+			
+	}
+	
+	private static void inicializar (){
+		System.out.println ("Bienvenido a procesador estadistico.");
+		pedirDirectorioDeMonitoreo();
+		generadorDeEstadisticas = new GeneradorDeEstadistica ();
+	}
+	
+	private static void menu (){
+		
+		String opcion = "";
+		
+		while (!"2".equals(opcion)){
+			System.out.println ("Ingrese opcion a realizar:");
+			System.out.println ("1- Procesar archivos de directorio especifico");
+			System.out.println ("2- salir");
+			opcion = escaner.next();
+			
+			switch (opcion){
+			case "1":comenzarProcesamiento();
+			break;
+			case "2": System.out.println ("Saliendo...");
+			break;
+			default : 
+				if (!"1".equals(opcion) && !"2".equals(opcion))
+					System.out.println("Opcion invalida");
+			}
+		}
+	}
+	
+	private static void comenzarProcesamiento (){ //ACA VA LA LOGICA DE PROCESAMIENTO
+		pedirDirectorioDeTrabajo ();
+		
+		
+		
+		
+		
+	}
+
+	private static void pedirDirectorioDeTrabajo()  {
+		
+		boolean configuracionCorrecta = false;
+		String directorioDeTrabajo = null;
+		
+		while (!configuracionCorrecta){
+			System.out.print ("Ingrese directorio de los archivos ZIP a procesar:");
+			directorioDeTrabajo = escaner.next();
+			configuracionCorrecta = comprobarExistenciaDeDirectorio (directorioDeTrabajo);
+		}
+		
+		//gestorDeArchivos = new GestorDeArchivos (directorioDeTrabajo);
+		
+	}
+
+	private static void pedirDirectorioDeMonitoreo() {
+		
+		boolean configuracionCorrecta = false;
+		String directorioDeMonitoreo = null;
+		
+		while (!configuracionCorrecta){
+			System.out.println ("");
+			System.out.println ("");
+			System.out.print ("Ingrese directorio a monitorear: ");
+			directorioDeMonitoreo = escaner.next();
+			configuracionCorrecta = comprobarExistenciaDeDirectorio (directorioDeMonitoreo);
+		}
+		monitorDeDirectorio = new MonitorDeDirectorio (directorioDeMonitoreo);
+	}
+
+	private static boolean comprobarExistenciaDeDirectorio(
+			String directorioDeTrabajo) {
+		
+		File directorioAEvaluar = new File (directorioDeTrabajo);
+		boolean existe = directorioAEvaluar.exists();
+		
+		if (!existe){
+			System.out.println ("No existe el directorio.");
+		}
+		
+		return existe;
+	}
 }
---
+++ src/utilidades/MonitorDeDirectorio.java
@@ -0,0 +1,16 @@
+package utilidades;
+
+public class MonitorDeDirectorio {
+
+	public MonitorDeDirectorio(String directorioDeMonitoreo) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void monitorear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
